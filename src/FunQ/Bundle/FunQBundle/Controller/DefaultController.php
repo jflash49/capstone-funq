165d7c7f83ec6dcfdf188982958a080823d9585b
jflash49 <otiscye531@hotmail.com>
2015-06-28 15:14:17 +0000

    changed bundle

--- src/FunQ/Bundle/FunQBundle/Controller/DefaultController.php
+++ src/FunQ/Bundle/FunQBundle/Controller/DefaultController.php
@@ -3,11 +3,26 @@
 namespace FunQ\Bundle\FunQBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+   /* public function indexAction($name)
     {
-        return $this->render('FunQFunQBundle:Default:index.html.twig');
+        return $this->render('FunQBundle:Default:index.html.twig', array('name' => $name));
+    }*/
+    
+    public function indexAction ( $name , $count)
+    {
+        $arr = array(
+            'First Name'=>$name,
+            'Count' => $count,
+            'status'=>'It\'s a trap!',
+            );
+        $response = new Response(json_encode($arr));
+        $response->headers->set('Content-Type','application/json');
+        return $response;
     }
+    
 }
+
---
+++ src/FunQ/Bundle/FunQBundle/FunQBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace FunQ\Bundle\FunQBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FunQBundle extends Bundle
+{
+}
